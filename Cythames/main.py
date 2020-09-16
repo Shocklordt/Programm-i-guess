1e2ac2062c6211d310fa7c87470c03a565461075
Arseniy Sergeenko <arseny.sergeenko@gmail.com>
2020-09-16 17:19:42 +0300

    updated from miironosuus.py

--- Cythames/main.py
+++ Cythames/main.py
@@ -16,28 +16,36 @@ class Widgetini (QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hei maailma"]
+        self.hello = "Hello World"
+        self.hei = "Hei Maailma"
 
         self.button = QtWidgets.QPushButton("For your clicking pleausre") 
-        self.text = QtWidgets.QLabel("Hello World")
+        self.text = QtWidgets.QLabel(self.hello)
         self.text.setAlignment(QtCore.Qt.AlignHCenter)
-
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
         self.setLayout(self.layout)
 
-        self.button.clicked.connect(self.magic)
+        self.button.clicked.connect(self.magic) # klikatessa nappia kutsuu magic funktiota
+        self.valinta = False # Boolean muuttuja, muuttaa painalluksen lopputulosta
 
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    def magic(self): # määritetään magic funktiota jossa on 2 vaihtoehtoa klikkauksen lopputuloksesta
+        if self.valinta == False: # jos valinta on False niin vaihda tekstiksi (self.hei)
+            self.text.setText(self.hei)
 
+        elif self.valinta == True: # jos valinta on True niin vaihda tekstiksi (self.hello)
+            self.text.setText(self.hello)
+
+        self.valinta = not(self.valinta) # self valinta vaihtuu, joka kerta päin vastaiseksi. Not komento sisältää itsessään vaihdon.
+
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = Widgetini()
-    widget.resize (800, 600)
+    widget.showMaximized() # Ikkuna avautuu näytön resoluutiossa
     widget.show()
 
     sys.exit(app.exec_())
