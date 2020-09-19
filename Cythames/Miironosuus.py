2d3518f7e7ca0328abe43228afe88a7adfe0a5ce
Miiro Pyysing <pyysingmiiro@outlook.com>
2020-09-19 12:21:11 +0300

    added miiro

--- Cythames/Miironosuus.py
+++ Cythames/Miironosuus.py
@@ -1,64 +1,61 @@
+'''
+This is a gui base for CYTHAMES
+Functionality will be added later
+
+VERSION 0.03
+
+'''
+
+
+
 import sys 
 import random
+import qdarkstyle
 from PySide2 import QtCore, QtWidgets, QtGui
 
 class Widgetini (QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.hei = "Hei maailma"
+
+          # setup darktheme
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
+        app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyside2"))
+
+        self.setWindowTitle("Cythames") # lisää ikkunalle tittelin
+
+        # määritetään funktiot, jotka sisältää pelkästään tekstin
         self.hello = "Hello World"
+        self.hei = "Hei Maailma"
 
-        self.button = QtWidgets.QPushButton("For your clicking pleausre")
+        # luodaan nappi
+        self.button = QtWidgets.QPushButton("Button 1") 
         self.text = QtWidgets.QLabel(self.hello)
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
+        self.text.setAlignment(QtCore.Qt.AlignHCenter)
 
-
+        # luodaan layout ohjelmalle
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
         self.setLayout(self.layout)
 
-        #klikkaus kutsuu magic fuktiota
-        self.button.clicked.connect(self.magic)
+        self.button.clicked.connect(self.magic) # klikatessa nappia kutsuu magic funktiota
+        self.valinta = False # Boolean muuttuja, muuttaa painalluksen lopputulosta
 
-        
-        # tehdään muuttuja jolla saadaan vaihdettua fuktion sisällä boolia / true / falsea 
-        self.valinta = False
-
-    # magic fuktio sisältää 2 vaihtoehtoa siitä mitä napin painalluksesta tapahtuu, riippuen siitä mikä testi on näkyvillä
-    def magic(self):
-
-        # jos valinta on False niin vaihda tekstiksi (self.hei)
-        if self.valinta == False:
+    def magic(self): # määritetään magic funktiota jossa on 2 vaihtoehtoa klikkauksen lopputuloksesta
+        if self.valinta == False: # jos valinta on False niin vaihda tekstiksi (self.hei)
             self.text.setText(self.hei)
 
-        # jos valinta on True niin vaihda tekstiksi (self.hello)    
-        elif self.valinta == True:
+        elif self.valinta == True: # jos valinta on True niin vaihda tekstiksi (self.hello)
             self.text.setText(self.hello)
-            
-        # self valinta vaihtuu, joka kerta päin vastaiseksi. NOT komento sisältää itsessään vaihdon.
-        self.valinta = not(self.valinta)      
 
+        self.valinta = not(self.valinta) # self valinta vaihtuu, joka kerta päin vastaiseksi. Not komento sisältää itsessään vaihdon.
 
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = Widgetini()
-    widget.resize (1920, 1080)
+    widget.showMaximized() # Ikkuna avautuu näytön resoluutiossa
     widget.show()
 
     sys.exit(app.exec_())
-
-'''
-     self.button.clicked.connect(self.magic)
-        
-        self.valinta = False
-
-    def magic(self):
-        if self.valinta == False:
-            self.text.setText(self.hei)
-        elif self.valinta == True:
-            self.text.setText(self.hello)
-
-        self.valinta = not(self.valinta) 
-'''
