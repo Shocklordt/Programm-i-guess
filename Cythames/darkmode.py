ec26b7e541dae8a8c8ab9e894f716e43b5564335
Arseniy Sergeenko <arseny.sergeenko@gmail.com>
2020-09-24 22:30:17 +0300

    Added Independent dark theme

--- Cythames/darkmode.py
+++ Cythames/darkmode.py
@@ -2,3 +2,17 @@ import sys
 import random 
 from PySide2 import QtCore, QtWidgets, QtGui
 
+'''
+self.stylesheet = """
+    QMainWindow {
+        background-color: #383738;
+    }
+    QPushButton {
+        background-color: #657e8a;
+        color: #ebdddd;
+        border: 2px solid #000000;
+        }
+    QLabel {
+        color: #ebdddd;
+            }
+'''
--- Cythames/main.py
+++ Cythames/main.py
@@ -6,56 +6,70 @@ VERSION 0.03
 
 '''
 
-
-
-import sys 
-import random
-import qdarkstyle
+import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
 class Widgetini (QtWidgets.QWidget):
+
     def __init__(self):
         super().__init__()
 
-          # setup darktheme
-        app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
-        app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyside2"))
+        self.setWindowTitle("Cythames")
 
-        self.setWindowTitle("Cythames") # lisää ikkunalle tittelin
+        # Ohjelman CSS teema
+        self.stylesheet = """
+            QWidget {
+                background-color: #383738;
+        }
+            QPushButton {
+                background-color: #657e8a;
+                color: #ebdddd;
+                border: 2px solid #000000;
+        }
+            QLabel {
+                color: #ebdddd;
+        }
+        """
 
-        # määritetään funktiot, jotka sisältää pelkästään tekstin
         self.hello = "Hello World"
-        self.hei = "Hei Maailma"
+        self.hei = "Hei maailma"
 
-        # luodaan nappi
-        self.button = QtWidgets.QPushButton("Button 1") 
+        self.button1 = QtWidgets.QPushButton("Button 1")
+        self.button2 = QtWidgets.QPushButton("Button 2")
+
         self.text = QtWidgets.QLabel(self.hello)
         self.text.setAlignment(QtCore.Qt.AlignHCenter)
 
-        # luodaan layout ohjelmalle
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.button1)
+        self.layout.addWidget(self.button2)
         self.setLayout(self.layout)
 
-        self.button.clicked.connect(self.magic) # klikatessa nappia kutsuu magic funktiota
-        self.valinta = False # Boolean muuttuja, muuttaa painalluksen lopputulosta
+        self.button1.clicked.connect(self.magic)
+        self.valinta = False
 
-    def magic(self): # määritetään magic funktiota jossa on 2 vaihtoehtoa klikkauksen lopputuloksesta
-        if self.valinta == False: # jos valinta on False niin vaihda tekstiksi (self.hei)
+        app.setStyleSheet(self.stylesheet)
+
+    def magic(self):
+        if self.valinta == False:
             self.text.setText(self.hei)
-
-        elif self.valinta == True: # jos valinta on True niin vaihda tekstiksi (self.hello)
+        
+        elif self.valinta == True:
             self.text.setText(self.hello)
 
-        self.valinta = not(self.valinta) # self valinta vaihtuu, joka kerta päin vastaiseksi. Not komento sisältää itsessään vaihdon.
+        self.valinta = not(self.valinta)
 
-        
+    
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+
+    app = QtWidgets.QApplication(sys.argv)
 
     widget = Widgetini()
-    widget.showMaximized() # Ikkuna avautuu näytön resoluutiossa
+
+    widget.showMaximized()
+
     widget.show()
+
 
     sys.exit(app.exec_())
