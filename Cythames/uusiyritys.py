d3ef5db91430b3f63839c1b007a30449d62959f4
Arseniy Sergeenko <arseny.sergeenko@gmail.com>
2020-09-25 13:17:07 +0300

    added more css options

--- Cythames/uusiyritys.py
+++ Cythames/uusiyritys.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QAction, QVBoxLayout
+from PySide2.QtWidgets import QApplication,QMainWindow, QAction, QVBoxLayout
 import sys
 from PySide2.QtGui import QIcon
  
@@ -18,8 +18,41 @@ class Window(QMainWindow):
             QLabel {
                 color: #ebdddd;
         }
-        """
+            QMenuBar {
+                background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                                            stop:0 lightgray, stop:1 darkgray);
+            }
+            QMenuBar::item {
+                spacing: 3px;           
+                padding: 2px 10px;
+                background-color: rgb(210,105,30);
+                color: rgb(255,255,255);  
+                border-radius: 5px;
+            }
+            QMenuBar::item:selected {    
+                background-color: #f70000;
+            }
+            QMenuBar::item:pressed {
+                background: #0026ff;
+            }
 
+            /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */  
+
+            QMenu {
+                background-color: #ABABAB;   
+                border: 1px solid black;
+                margin: 2px;
+            }
+            QMenu::item {
+                background-color: transparent;
+            }
+            QMenu::item:selected { 
+                background-color: #654321;
+                color: rgb(255,255,255);
+            }
+            """
+        
+        self.setStyleSheet(self.stylesheet)
         self.setWindowTitle("Cythames")
         self.showMaximized()
         self.create_menu()
