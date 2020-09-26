c09b22a692a0c407d83544529e5fc5c6975d0999
Miiro Pyysing <pyysingmiiro@outlook.com>
2020-09-26 17:58:13 +0300

    Added Miiro

--- Cythames/Miironosuus.py
+++ Cythames/Miironosuus.py
@@ -1,12 +1,21 @@
-from PySide2.QtWidgets import QApplication,QMainWindow, QAction, QVBoxLayout
+from PySide2.QtWidgets import QApplication,QMainWindow, QAction, QVBoxLayout, QPushButton
 import sys
 from PySide2.QtGui import QIcon
  
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        # Ohjelman CSS teema
-        self.stylesheet = """
+        
+        self.style()
+        self.setWindowTitle("Cythames")
+        self.showMaximized()
+        self.create_menu()
+        self.nappula()
+        self.show()        
+
+    def style(self):
+
+        self.setStyleSheet ("""
             QWidget {
                 background-color: #383738;
         }
@@ -18,17 +27,44 @@ class Window(QMainWindow):
             QLabel {
                 color: #ebdddd;
         }
-
             QMenuBar {
-                color: #ffffff
-        }
-        """
-        self.setStyleSheet(self.stylesheet)
-        self.setWindowTitle("Cythames")
-        self.showMaximized()
-        self.create_menu()
-        self.show()
- 
+                background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                                            stop:0 lightgray, stop:1 darkgray);
+            }
+            QMenuBar::item {
+                spacing: 3px;           
+                padding: 2px 10px;
+                background-color: #294d29;
+                color: rgb(255,255,255);  
+                border-radius: 5px;
+            }
+            QMenuBar::item:selected {    
+                background-color: #f70000;
+            }
+            QMenuBar::item:pressed {
+                background: #0026ff;
+            }
+
+            /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */  
+
+            QMenu {
+                background-color: #ABABAB;   
+                border: 1px solid black;
+                margin: 2px;
+            }
+            QMenu::item {
+                background-color: transparent;
+            }
+            QMenu::item:selected { 
+                background-color: #1ce81c;
+                color: rgb(255,255,255);
+            }
+            QMenu::item:pressed {
+                background-color: #e81cb8
+                color: #00f2ff
+            }
+            """)
+
     def create_menu(self):
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu("File")
@@ -39,8 +75,13 @@ class Window(QMainWindow):
         exitAction.triggered.connect(self.exit_app)
  
         fileMenu.addAction(exitAction)
-        
-        
+
+    def nappula (self):
+        nappi = QPushButton("Nappi",self)
+        nappi.resize(100,100)
+        nappi.move(100,100)
+        self.show()
+
     def exit_app(self):
         self.close()
  
